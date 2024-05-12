8d6f291026900c755ca20ca4303da4cb2c4d779e
Dihad Hasan <130214955+Dihad@users.noreply.github.com>
2024-05-12 11:02:12 +0600

    Add files via upload

---
+++ Sqrt(x).py
@@ -0,0 +1,14 @@
+import math
+num = int(input('enter the number: '))
+current_valid_num = 0
+for checking_num in range(num):
+    current_valid_num_sqr = checking_num * checking_num
+    if current_valid_num_sqr == num:
+        print(current_valid_num)
+        break
+    if current_valid_num_sqr < num:
+        current_valid_num = checking_num
+    if current_valid_num_sqr > num:
+        print(current_valid_num)
+        break
+    
