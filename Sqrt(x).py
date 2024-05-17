9d6d9bd5299396cb0ed40d1cb1c893b0b6461ee7
Dihad Hasan <130214955+Dihad@users.noreply.github.com>
2024-05-18 00:12:48 +0600

    Sqrt(x).py

--- Sqrt(x).py
+++ Sqrt(x).py
@@ -4,7 +4,7 @@ current_valid_num = 0
 for checking_num in range(num):
     current_valid_num_sqr = checking_num * checking_num
     if current_valid_num_sqr == num:
-        print(current_valid_num)
+        print(checking_num )
         break
     if current_valid_num_sqr < num:
         current_valid_num = checking_num
